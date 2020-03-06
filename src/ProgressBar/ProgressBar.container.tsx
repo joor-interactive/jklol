67125ad949aad2ad5690292c961b4b4e85239513
Scott <scottywags16@gmail.com>
2020-03-06 18:05:19 -0500

    change progress bar to allow it to fill

--- src/ProgressBar/ProgressBar.container.tsx
+++ src/ProgressBar/ProgressBar.container.tsx
@@ -1,20 +1,24 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { scan } from 'rxjs/operators'
-import { AppContext } from '../App'
-import ProgressBar from './ProgressBar.component'
+import React, { useState, useEffect, useContext } from "react";
+import { scan } from "rxjs/operators";
+import { AppContext } from "../App";
+import ProgressBar from "./ProgressBar.component";
 
 const ProgressBarContainer = () => {
-    const { keyPressObservable } = useContext(AppContext)
-    const [progressAmount, setProgressAmount] = useState(0)
-    useEffect(() => {
-        keyPressObservable.pipe(scan((acc, e) => (acc + 0.03) % 1, 0))
-        .subscribe((value) => {
-            setProgressAmount(value)
-        })
-    }, [])
-    return (
-        <ProgressBar progressAmount={progressAmount} />
-    );
+  const { keyPressObservable } = useContext(AppContext);
+  const [progressAmount, setProgressAmount] = useState(0);
+  useEffect(() => {
+    keyPressObservable
+      .pipe(
+        scan(acc => {
+          const number = Number((acc + 0.02).toFixed(2));
+          return number === 1 ? 1 : number % 1;
+        }, 0)
+      )
+      .subscribe(value => {
+        setProgressAmount(value);
+      });
+  }, [keyPressObservable]);
+  return <ProgressBar progressAmount={progressAmount} />;
 };
 
 export default ProgressBarContainer;
