b5ed9bc124aa67206b958903548d9ef72ee62cf2
Douglas Naphas <douglasnaphas@gmail.com>
2020-03-06 18:11:56 -0500

    Only look for dave in the test
    https://github.com/joor-interactive/jklol/issues/1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/HIIIIIIII/i);
+  const linkElement = getByText(/dave/i);
   expect(linkElement).toBeInTheDocument();
 });
