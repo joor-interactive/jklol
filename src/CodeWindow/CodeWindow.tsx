8498ab0777d06e3a7afd4322099e4a6c8c245557
Scott <scottywags16@gmail.com>
2020-03-06 20:07:44 -0500

    make CodeWindow handle multiple snippets in multiple languages

--- src/CodeWindow/CodeWindow.tsx
+++ src/CodeWindow/CodeWindow.tsx
@@ -1,11 +1,19 @@
-import React, { useContext, useState, useEffect } from "react"
-import Prism from 'prismjs'
-import { useRequestChars } from "../TextHanders/basicHandler"
-import { AppContext } from "../App";
-import { scan, map } from 'rxjs/operators';
+import React, { useContext, useState, useEffect } from "react";
+import Prism from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-python";
 
-const javascript = `
-const express = require('express')\n
+import { AppContext } from "../App";
+import { scan } from "rxjs/operators";
+
+type Snippet = {
+  language: string;
+  code: string;
+};
+const javascript: Snippet = {
+  language: "javascript",
+  code: `const express = require('express')\n
 \tconst app = express()\n
 \tconst port = 3000\n
 
@@ -13,27 +21,99 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log('Example app listening...'))
 `
+};
 
-const CodeWindow = ({charactersPerKeystroke = 3}) => {
-    const [text, setText] = useState<string>("")
+const python: Snippet = {
+  language: "python",
+  code: `import sys
+import calendar
+ 
+year = 2013
+if len(sys.argv) > 1:
+    try:
+        year = int(sys.argv[-1])
+    except ValueError:
+        pass
+ 
+for month in range(1, 13):
+    last_sunday = max(week[-1] for week in calendar.monthcalendar(year, month))
+    print('{}-{}-{:2}'.format(year, calendar.month_abbr[month], last_sunday))
+`
+};
 
-    const {keyPressObservable} = useContext(AppContext);
+const snippets: Record<string, Snippet> = {
+  javascript,
+  python
+};
 
-    useEffect(() => {
-        keyPressObservable
-            .pipe(scan((acc, e) => (acc + charactersPerKeystroke) % javascript.length, 0))
-            .subscribe((e: number) => {
-                setText((curr: string) => curr + javascript.substring(e - charactersPerKeystroke, e))
-            })
-    }, [keyPressObservable, charactersPerKeystroke])
-    
-    // when you are ready to emit progress... 
-    // snippetProgress.next(progress);
+const getRandomSnippet = () =>
+  Object.values(snippets)[
+    Math.floor(Math.random() * Object.keys(snippets).length)
+  ];
 
-    const html = Prism.highlight(text, Prism.languages.javascript, 'javascript')
-    return (
-    <pre><code className="language-javascript" dangerouslySetInnerHTML={{ __html: html }}></code></pre>
-    )
-}
+const CodeWindow = ({ charactersPerKeystroke = 3 }) => {
+  const [texts, setTexts] = useState<Snippet[]>([
+    { language: "javascript", code: "" }
+  ]);
+  const [snippet, setActiveSnippet] = useState(javascript);
 
-export default CodeWindow
+  const { keyPressObservable } = useContext(AppContext);
+
+  useEffect(() => {
+    const observable = keyPressObservable
+      .pipe(
+        scan(
+          (acc, e) => (acc + charactersPerKeystroke) % snippet.code.length,
+          0
+        )
+      )
+      .subscribe(e => {
+        if (e + charactersPerKeystroke >= snippet.code.length) {
+          const nextSnippet = getRandomSnippet();
+          setActiveSnippet(nextSnippet);
+          setTexts(curr => {
+            const [, ...rest] = curr;
+            return [
+              { language: nextSnippet.language, code: "" },
+              snippet,
+              ...rest
+            ];
+          });
+        } else {
+          setTexts((curr: Snippet[]) => {
+            const [head, ...rest] = curr;
+            const newHead = {
+              language: head.language,
+              code:
+                head.code +
+                snippet.code.substring(e - charactersPerKeystroke, e)
+            };
+            return [newHead, ...rest];
+          });
+        }
+      });
+    return () => observable.unsubscribe();
+  }, [keyPressObservable, charactersPerKeystroke, snippet]);
+
+  return (
+    <>
+      {[...texts].reverse().map((text, index) => {
+        const html = Prism.highlight(
+          text.code,
+          Prism.languages[text.language],
+          text.language
+        );
+        return (
+          <pre className={`language-${text.language}`} key={index}>
+            <code
+              className={`language-${text.language}`}
+              dangerouslySetInnerHTML={{ __html: html }}
+            ></code>
+          </pre>
+        );
+      })}
+    </>
+  );
+};
+
+export default CodeWindow;
