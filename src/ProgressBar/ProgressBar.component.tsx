546e6855902f50d271f529326839295785007b95
Scott <scottywags16@gmail.com>
2020-03-06 21:29:41 -0500

    make progress bar full page width

--- src/ProgressBar/ProgressBar.component.tsx
+++ src/ProgressBar/ProgressBar.component.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const totalWidth = 500
+const totalWidth = 100;
 
 type ProgressBarProps = {
-    progressAmount: number
-}
+  progressAmount: number;
+};
 const ProgressBar: React.FC<ProgressBarProps> = ({ progressAmount }) => {
-
-    const rgb = `rgb(${255*(1-progressAmount)},${255*progressAmount},0)`;
-    return (
-        <div style={{ position: 'relative', width: `${totalWidth}px`, height: '40px', background: 'white', border: '1px solid black'}}>
-            <div style={{ position: 'absolute', width: `${progressAmount * totalWidth}px`, height: '40px', background: rgb}}></div>
-        </div>
-    );
+  const rgb = `rgb(${255 * (1 - progressAmount)},${255 * progressAmount},0)`;
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: `${totalWidth}vw`,
+        height: "40px",
+        background: "white",
+        border: "1px solid black"
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: `${progressAmount * totalWidth}vw`,
+          height: "40px",
+          background: rgb
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default ProgressBar;
